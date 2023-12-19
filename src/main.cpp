b57d6c5246707261b1834d59f84a73c34c4ca7f9
stefan-dnc <dinca.stefan960@gmail.com>
2023-12-20 00:32:48 +0200

    FPS COUNTER

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <stdlib.h>
 
 Player player;
 
@@ -13,18 +14,32 @@ int main()
 
     constexpr int framesPerSecond = 60;
     constexpr std::chrono::duration<double> frameDuration(1.0 / framesPerSecond);
+    int i = 1;
+
+    system("clear");
 
     while (true)
     {
         // Clear the screen escape sequence
-        std::cout << "\033[2J\033[H";
+        if (i % framesPerSecond != 0)
+        {
+            std::cout << "\033[" << (i / framesPerSecond) + 1 << ";1H";
+            std::cout << "\033[K";
+            // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+        // std::cout << "\033[2J\033[H";
 
         // Your rendering or refreshing logic goes here
-        std::cout << "Frame rendered at " << framesPerSecond << "fps" << std::endl;
+        std::cout << "Frame rendered at " << i << "fps" << '\n';
+        i++;
 
         // Sleep for the remaining time to achieve the desired frame rate
         std::this_thread::sleep_for(frameDuration);
     }
+    /*std::cout << "abcdef";
+    std::cout << "\033[" << (i / 60) << ";0H";
+    std::cout << "\033[K";
+    std::cout << "exemplu";*/
 
     // Menu menu(window);
     // menu.draw();
